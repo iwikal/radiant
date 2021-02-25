17d64304093fcd7ebc382e6236e9cc31f32da727
Joel Nordström <e.joel.nordstrom@gmail.com>
2021-02-25 04:13:55 +0100

    Add benchmark

---
+++ benches/bench.rs
@@ -0,0 +1,17 @@
+#![feature(test)]
+
+extern crate test;
+
+use std::io::BufReader;
+use std::fs::File;
+
+use test::Bencher;
+
+#[bench]
+fn bench(b: &mut Bencher) {
+    b.iter(|| {
+        let f = File::open("assets/colorful_studio_2k.hdr").unwrap();
+        let f = BufReader::new(f);
+        hdrldr::load(f).unwrap()
+    });
+}
