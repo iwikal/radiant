4e4cb7f9721f7942a20c1d5aa08a8dcf1983dbc6
Joel Nordström <e.joel.nordstrom@gmail.com>
2021-02-26 23:12:37 +0100

    Don't benchmark fs

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,16 +2,10 @@
 
 extern crate test;
 
-use std::fs::File;
-use std::io::BufReader;
-
 use test::Bencher;
 
 #[bench]
 fn bench(b: &mut Bencher) {
-    b.iter(|| {
-        let f = File::open("assets/colorful_studio_2k.hdr").unwrap();
-        let f = BufReader::new(f);
-        radiant::load(f).unwrap()
-    });
+    let f = &include_bytes!("../assets/colorful_studio_2k.hdr")[..];
+    b.iter(|| radiant::load(f).unwrap());
 }
